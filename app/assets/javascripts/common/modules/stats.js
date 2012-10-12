bb635c02235e9a9e88b0e58e0fe6441d6901a840
Daniel Valencia <daniel.valencia@koombea.com>
2012-10-12 10:44:35 -0500

    removing active_record
    Signed-off-by: Daniel Valencia <daniel.valencia@koombea.com>

--- app/assets/javascripts/common/modules/stats.js
+++ app/assets/javascripts/common/modules/stats.js
@@ -1,5 +1,12 @@
 Stats = function(){
-  
+  var self = this;
+  var googleWrapper = null;
+
+  function initialize(google){
+    googleWrapper = google;
+    
+  }
+
   return {
     initialize: initialize
   }
